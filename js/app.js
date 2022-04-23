ef44be075ba577159f5504a833640c1e3b6a3bd8
Sander <sanderjverhoeven@hotmail.com>
2022-04-23 22:27:53 +0200

    added an about me

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
+let sendToTop = document.getElementById("logo")
+
+sendToTop.addEventListener('click', toTop)
+
+// When the user clicks on the button, scroll to the top of the document
+function toTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// used for the typing animation
 window.ityped.init(document.querySelector('.ityped'), {
     strings: ["Sander Verhoeven"],
     loop: true,
     typeSpeed: 200,
-    backSpeed:  100,
+    backSpeed: 100,
 })
