7e351db2f03b088a3e631e9850d8e18abf745bd4
Sander <sanderjverhoeven@hotmail.com>
2022-04-24 16:39:11 +0200

    added smooth scrolling/ navigation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,57 @@
-let sendToTop = document.getElementById("logo")
+let homeNav = document.getElementById("homeNav");
+let aboutNav = document.getElementById("aboutNav");
+let projectsNav = document.getElementById("projectsNav")
+let contactNav = document.getElementById("contactNav")
 
-sendToTop.addEventListener('click', toTop)
+
+homeNav.addEventListener("click", function () {
+    toTop(homeNav.innerHTML);
+});
+aboutNav.addEventListener("click", function () {
+    toTop(aboutNav.innerHTML);
+});
+projectsNav.addEventListener("click", function () {
+    toTop(projectsNav.innerHTML);
+});
+contactNav.addEventListener("click", function () {
+    toTop(contactNav.innerHTML);
+});
 
 // When the user clicks on the button, scroll to the top of the document
-function toTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+function toTop(name) {
+    let height = document.querySelector('#navBar').offsetHeight
+    console.log(height)
+    switch (name) {
+        case 'Sander':
+            let content = document.getElementById("content")
+            let contentPos = content.offsetTop
+            window.scrollTo(0,  contentPos);
+            break;
+
+        case 'About':
+            let about = document.getElementById("about")
+            let aboutPos = about.offsetTop - height;
+            window.scrollTo(0,  aboutPos);
+            break;
+
+        case 'Projects':
+            let projects = document.getElementById("projects")
+            let projectsPos = projects.offsetTop - height;
+            window.scrollTo(0,  projectsPos);
+            break;
+
+        case 'Contact':
+            console.log('Contact')
+            break;
+
+        default:
+            console.log('no')
+            break;
+    }
+    // window.scrollTo({
+    //     top: 0,
+    //     behavior: 'smooth'
+    // });
 }
 
 // used for the typing animation
